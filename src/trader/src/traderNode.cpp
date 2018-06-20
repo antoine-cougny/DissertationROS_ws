4a0ee4b313c8650f939a26fde23d437829cf442a
Antoine <aoc2@hw.ac.uk>
2018-06-20 10:31:40 +0100

    added traderNode.cpp file

---
+++ src/trader/src/traderNode.cpp
@@ -0,0 +1,12 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_srvs/Trigger.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/Pose.h"
+#include "trader/Task.h"
+#include "trader/metrics.h"
+
+int main(int argv, char **argc)
+{
+    return 0;
+}
