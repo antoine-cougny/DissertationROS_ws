772f91151c49a57c88e154f7669994df576bee80
Antoine <aoc2@hw.ac.uk>
2018-06-21 15:02:31 +0100

    build structure of topics

--- src/trader/src/decisionNode.cpp
+++ src/trader/src/decisionNode.cpp
@@ -2,6 +2,7 @@
 // Ros
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Bool.h"
 #include "std_srvs/Trigger.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Pose.h"
@@ -17,6 +18,22 @@ using namespace std;
 int idRobot = 0;
 string ns;
 
+/*******************************************************************************
+ *
+ * Basically, what this node does is store an array of task to perform.
+ * It will pick one from the list, check its metrics and based on a treshold
+ * value, send it to the task exec or to the trader to get rid of it.
+ *
+ * If the task list is empty, the node will advertise the robot as idle
+ *
+ ******************************************************************************/
+
+void newTask_cb(const trader::Task &msg)
+{
+    // Do some stuff like adding it to an array or a vector
+    // Pay attention to pointers and content of the message
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "traderRobot");
@@ -25,6 +42,24 @@ int main(int argc, char **argv)
 
     // For the n NodeHandle, we get param from the launch file
     n.param<int>("idRobot", idRobot, 0);
+
+    // Advertise it there are tasks saved
+    ros::Publisher hasTasks_pub;
+    hasTasks_pub = n.advertise<std_msgs::Bool>("hasTasks", 1);
+    // Sending goal to the task exec
+    ros::Publisher sendGoal_pub;
+    sendGoal_pub = n.advertise<trader::Task>("sendGoal", 1);
+    // Send a goal to be traded
+    ros::Publisher taskToTrade_pub;
+    taskToTrade_pub = n.advertise<trader::Task>("taskToTrade", 2);
+
+    // Getting a new task from traderNode
+    ros::Subscriber newTask_sub = n.subscribe("newTask", 1, newTask_cb);
+
+    // Metrics Service client, to get the metrics value
+    ros::ServiceClient metrics_srvC;
+    metrics_srvC = n.serviceClient<trader::metrics>("metricsNode");
+
 
     while (ros::ok())
     {
--- src/trader/src/traderNode.cpp
+++ src/trader/src/traderNode.cpp
@@ -2,6 +2,7 @@
 // Ros
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Bool.h"
 #include "std_srvs/Trigger.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Pose.h"
@@ -15,6 +16,8 @@
 using namespace std;
 
 int idRobot = 0;
+bool isIdle = true,
+     hasTasks = false;
 string ns;
 
 void announcement_cb(const trader::announcement &msg)
@@ -22,10 +25,21 @@ void announcement_cb(const trader::announcement &msg)
     ROS_INFO("got msg");
 }
 
-void decision_cb(const trader::task)
+void taskToTrade_cb(const trader::Task &msg)
 {
     // Do stuff
 }
+
+void isIdle_cb(const std_msgs::Bool &msg)
+{
+    isIdle = msg.data;
+}
+
+void hasTasks_cb(const std_msgs::Bool &msg)
+{
+    hasTasks = msg.data;
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "traderRobot");
@@ -43,9 +57,18 @@ int main(int argc, char **argv)
     announcement_pub = n.advertise<trader::announcement>("/announcementGlobal", 1);
     announcement_sub = n.subscribe("/announcementGlobal", 1, announcement_cb);
 
+    // Getting robot state from decisionNode and taskExec
+    ros::Subscriber isIdle_sub = n.subscribe("isIdle", 1, isIdle_cb);
+    ros::Subscriber hasTasks_sub = n.subscribe("hasTasks", 1, hasTasks_cb);
+
     // Getting task from the decision node
-    ros::Subscriber decision_sub;
-    decision_sub = n.subscribe("decisionNode", 1, decision_cb);
+    ros::Subscriber taskToTrade_sub = n.subscribe("taskToTrade", 2, taskToTrade_cb);
+
+    // Sending a new task to decisionNode
+    ros::Publisher newTask_pub = n.advertise<trader::Task>("newTask", 1);
+
+    // Variables declaration
+    bool is_task_available_for_trading = false;
     
     while(ros::ok())
     {
